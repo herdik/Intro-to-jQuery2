0138acf5e9b904e6af90c57dc43d976254bc92d1
herdik <juraj.herda1983@gmail.com>
2022-01-24 15:58:06 +0100

    finish

---
+++ scripts.js
@@ -0,0 +1,21 @@
+let color = ['purple'];
+let number = 10;
+let word = ['cool'];
+
+if (color === color) {
+$('.north').css('background','purple');
+}
+
+if (number >= 100) {
+$('.east').text("whoah, thats' a big number");
+} else {
+    $('.east').text('just a regular number, please');
+}
+
+if (word === word) {
+    $('.south').text("Power of DOM");
+} else {
+    $('.west').text("Power of DOM");
+}
+
+
